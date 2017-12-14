41f9be1363227118636049e36512d44db3fcc842
lhzq <1187454868@qq.com>
2017-12-14 11:05:18 +0800

    lihuan 安全测试

--- app/src/main/java/com/jimtrency/lh/lhandroidframe/constant/Net.java
+++ app/src/main/java/com/jimtrency/lh/lhandroidframe/constant/Net.java
@@ -8,6 +8,5 @@ public class Net {
     public static boolean isTest = false;// 是否为测试环境
 
     //服务器地址 http://183.230.101.153:18080/
-    public static String SERVER_URL =
-            isTest ? "http://113.204.229.74:6888/" : "https://iosapi.12582.cn/";
+
 }
